896349eeaf9bf62914c995e359d969001fd6e0c6
wkRonin <912370908@qq.com>
2021-06-12 21:09:33 +0800

    hogwarts: 0612第二题

--- dailyquestiion/ques0612.py
+++ dailyquestiion/ques0612.py
@@ -25,3 +25,31 @@ print('所有字母大写: ' + hogwarts.upper())
 print('所有字母小写: ' + hogwarts.lower())
 print('首字母大写，其余都小写： ' + hogwarts.capitalize())
 print('大写转小写，小写转大写： ' + hogwarts.swapcase())
+
+# 数据转换题
+test = [
+    {'fQY': 'A', 'fWD_MC': 'a1'},
+    {'fQY': 'A', 'fWD_MC': 'a2'},
+    {'fQY': 'A', 'fWD_MC': 'a3'},
+    {'fQY': 'B', 'fWD_MC': 'b1'},
+    {'fQY': 'C', 'fWD_MC': 'c1'},
+    {'fQY': 'C', 'fWD_MC': 'c2'}
+]
+
+# 预期结果：
+# result= [
+#     {'name': 'A', 'children': [{'name': 'a1'}, {'name': 'a2'}, {'name': 'a3'}]},
+#     {'name': 'B', 'children': [{'name': 'b1'}]},
+#     {'name': 'C', 'children': [{'name': 'c1'}, {'name': 'c2'}]}
+# ]
+res = []
+for item in test:
+    for data in res:
+        if data['name'] == item['fQY']:
+            data['children'].append({'name': item['fWD_MC']})
+            break
+    else:
+        res.append({'name': item['fQY'], 'children': [{'name': item['fWD_MC']}]})
+print(f'结果：{res}')
+
+
